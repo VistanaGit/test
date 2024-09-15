0ebbc2dc76205a045cd71213e6d3a9325a2193c7
VistanaGit <90600707+VistanaGit@users.noreply.github.com>
2024-09-15 13:39:18 +0330

    Update db_configure.py

--- db_configure.py
+++ db_configure.py
@@ -1,13 +1,9 @@
-
-# To define your database engine, session, and base class.
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database connection
-DATABASE_URL = "mysql+pymysql://rd_db_usr:rd_pass@localhost/RD_DB_3"
+DATABASE_URL = "mysql+pymysql://rd_db_usr:rd_pass@db_host:db_port/RD_DB_3"
 
 # Create engine
 engine = create_engine(DATABASE_URL)
