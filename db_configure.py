5417521cfdd0e14cfd3d35a8e42ea202e43e8753
VistanaGit <90600707+VistanaGit@users.noreply.github.com>
2024-09-15 13:46:33 +0330

    Update db_configure.py

--- db_configure.py
+++ db_configure.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database connection
-DATABASE_URL = "mysql+pymysql://rd_db_usr:rd_pass@db_host:db_port/RD_DB_3"
+# Hardcoded example with correct port number
+DATABASE_URL = "mysql+pymysql://rd_db_usr:rd_pass@db_host:3306/RD_DB_3"
 
 # Create engine
 engine = create_engine(DATABASE_URL)
@@ -13,3 +13,4 @@ Base = declarative_base()
 
 # Create session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
