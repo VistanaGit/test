f3bdf5352cf47a2429831123bc750330ea01540d
VistanaGit <90600707+VistanaGit@users.noreply.github.com>
2024-09-15 18:13:53 +0330

    Update db_configure.py

--- db_configure.py
+++ db_configure.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database connection
-# DATABASE_URL = "postgresql+psycopg2://amdb_user:bkk2ZKn3eESLEoojzTNpJwWDR67eqcBo@dpg-crjenqe8ii6s73fffvjg-a:5432/amdb"
-DATABASE_URL = postgresql://amdb_user:bkk2ZKn3eESLEoojzTNpJwWDR67eqcBo@dpg-crjenqe8ii6s73fffvjg-a/amdb
+DATABASE_URL = "postgresql+psycopg2://amdb_user:bkk2ZKn3eESLEoojzTNpJwWDR67eqcBo@dpg-crjenqe8ii6s73fffvjg-a:5432/amdb"
+
 
 # Create engine
 engine = create_engine(DATABASE_URL)
