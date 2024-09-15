79f43985c199ca17220b391049e7207a69710e50
VistanaGit <90600707+VistanaGit@users.noreply.github.com>
2024-09-15 17:52:40 +0330

    Update initialize_db.py

--- initialize_db.py
+++ initialize_db.py
@@ -1,10 +1,8 @@
-
-# Step 3: Create the Tables Using db_configure.py
-# In the main application file or an initialization script, you can create the tables using the Base and engine from db_configure.py.
-
-
 from db_configure import engine, Base
 import models  # Ensure the models are imported so that Base has all model definitions
+
+# Drop all tables if they exist
+Base.metadata.drop_all(bind=engine)
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
