8ec487083e64a17d18411316d721a6a3daa2371c
VistanaGit <90600707+VistanaGit@users.noreply.github.com>
2024-09-22 13:28:03 +0330

    Update db_configure.py

--- db_configure.py
+++ db_configure.py
@@ -1,19 +1,27 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database connection
-DATABASE_URL = "postgresql+psycopg2://amdb_user:bkk2ZKn3eESLEoojzTNpJwWDR67eqcBo@dpg-crjenqe8ii6s73fffvjg-a:5432/amdb"
-
+DATABASE_URL = "postgresql+psycopg2://amuser:ampass@localhost:5432/amdb"
 
 # Create engine
-engine = create_engine(DATABASE_URL)
+try:
+    engine = create_engine(DATABASE_URL)
+    print("Database engine created successfully.")
+except Exception as e:
+    print(f"Error creating database engine: {e}")
 
 # Create base class for models
-Base = declarative_base()
+try:
+    Base = declarative_base()
+    print("Base class for models created successfully.")
+except Exception as e:
+    print(f"Error creating base class for models: {e}")
 
 # Create session factory
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
+try:
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    print("Session factory created successfully.")
+except Exception as e:
+    print(f"Error creating session factory: {e}")
